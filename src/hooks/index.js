23a9402c797a16e92a23309f6ca081ddef70432d
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-02-21 20:26:40 +0200

    adding useResource custom-hook and needed functionality for external communication

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,8 +1,27 @@
-import { useState } from 'react';
-import axios from axios
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
-export const useResource=(url)=>{
+export const useResource=(baseUrl)=>{
+  const [resources,setResources]=useState([])
 
+  useEffect(()=>{
+    axios.get(baseUrl)
+    .then(res=>{
+      //console.log(res);
+     setResources(res.data)})
+    })
+
+  const create=(newObject)=>{
+    axios.post(baseUrl,newObject)
+    .then(res=>setResources(resources.concat(res.data)))
+    .catch(err=>console.log('err'))
+  }
+
+  const service={
+    create
+  }
+
+  return[resources,service]
 
 }
 
